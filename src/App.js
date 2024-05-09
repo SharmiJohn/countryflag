00e545f6f31ea4cc8e63740b6e7d2e10b97dca1d
SharmiJohn <1422000joselvi@gmail.com>
2024-05-09 14:36:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios"
+import style from "./App.module.css"
+
+
+function Tile({flagname,altname,name}) {
+  return (
+    <div >
+      <img src={flagname} alt={altname}/>
+      <h3>{name}</h3>
+    </div>
+  )
+}
+
+
 
 function App() {
+  const [countrydata,setcountrydata]=useState([]);
+  useEffect(()=>{
+    const fetchdata=async()=>{
+      try{ 
+        const response=await axios.get("https://restcountries.com/v3.1/all")
+       // console.log(response.data);
+        setcountrydata(response.data)
+       
+      }
+      catch(error){
+        console.log(error);
+      }
+    }
+   
+    fetchdata();
+   
+  },[])
+  console.log(countrydata);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+    <div className={style.data} >
+      <div>
+        {countrydata.map((data,index)=><Tile key={index} flagname={data.flags.png} altname={data.flags.alt} name={data.name.common} />)}
+       
+      </div>
+    </div>
+
     </div>
   );
 }
